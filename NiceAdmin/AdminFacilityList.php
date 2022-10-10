5bf50e39ab80c648bc959c2fe56648d5675ba024
dehkai <ydk1421@gmail.com>
2022-10-10 16:27:48 +0800

    Update AdminFacilityList.php

--- NiceAdmin/AdminFacilityList.php
+++ NiceAdmin/AdminFacilityList.php
@@ -282,14 +282,14 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
       <li class="nav-item">
-        <a class="nav-link " href="homepage.php">
+        <a class="nav-link collapsed" href="homepage.php">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
       </li><!-- End Dashboard Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+        <a class="nav-link" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
            <i class="bi bi-menu-button-wide"></i><span>Facilities</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
         <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
