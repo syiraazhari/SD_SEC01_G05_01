bd85abdbe0dd3a2dfd28289156358f423454f81a
dehkai <ydk1421@gmail.com>
2022-11-15 01:33:22 +0800

    Update admin and student page ui

--- NiceAdmin/ViewStudentAccount.php
+++ NiceAdmin/ViewStudentAccount.php
@@ -222,98 +222,6 @@
         </ul>
       </li><!-- End Facilities Nav -->
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-journal-text"></i><span>Forms</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="forms-elements.html">
-              <i class="bi bi-circle"></i><span>Form Elements</span>
-            </a>
-          </li>
-          <li>
-            <a href="forms-layouts.html">
-              <i class="bi bi-circle"></i><span>Form Layouts</span>
-            </a>
-          </li>
-          <li>
-            <a href="forms-editors.html">
-              <i class="bi bi-circle"></i><span>Form Editors</span>
-            </a>
-          </li>
-          <li>
-            <a href="forms-validation.html">
-              <i class="bi bi-circle"></i><span>Form Validation</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Forms Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-layout-text-window-reverse"></i><span>Tables</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="tables-general.html">
-              <i class="bi bi-circle"></i><span>General Tables</span>
-            </a>
-          </li>
-          <li>
-            <a href="tables-data.html">
-              <i class="bi bi-circle"></i><span>Data Tables</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Tables Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-bar-chart"></i><span>Charts</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="charts-chartjs.html">
-              <i class="bi bi-circle"></i><span>Chart.js</span>
-            </a>
-          </li>
-          <li>
-            <a href="charts-apexcharts.html">
-              <i class="bi bi-circle"></i><span>ApexCharts</span>
-            </a>
-          </li>
-          <li>
-            <a href="charts-echarts.html">
-              <i class="bi bi-circle"></i><span>ECharts</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Charts Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-gem"></i><span>Icons</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="icons-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="icons-bootstrap.html">
-              <i class="bi bi-circle"></i><span>Bootstrap Icons</span>
-            </a>
-          </li>
-          <li>
-            <a href="icons-remix.html">
-              <i class="bi bi-circle"></i><span>Remix Icons</span>
-            </a>
-          </li>
-          <li>
-            <a href="icons-boxicons.html">
-              <i class="bi bi-circle"></i><span>Boxicons</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Icons Nav -->
-
       <li class="nav-heading">Pages</li>
 
       <li class="nav-item">
@@ -509,13 +417,6 @@
                     ?>
                   </div>
 
-                  <div class="row">
-                    <div class="col-lg-3 col-md-4 label">password</div>
-                    <?php
-                    echo '<div class="col-lg-9 col-md-8">'.$password.'</div>'
-
-                    ?>
-                  </div>
 
                   <div class="row">
                     <div class="col-lg-3 col-md-4 label">User Category</div>
@@ -579,16 +480,6 @@
                       <?php
                          echo '<div class="col-lg-9 col-md-8">'.$email.'</div>';
                           echo '<input name="email" type="hidden" class="form-control" id="fullName" value="'.$email.'"readonly>';
-                        ?>
-                      </div>
-                    </div>
-
-                    <div class="row mb-3">
-                      <label for="Address" class="col-md-4 col-lg-3 col-form-label">Password</label>
-                      <div class="col-md-8 col-lg-9">
-                      <?php
-                          
-                          echo '<input name="password" type="text" class="form-control" id="fullName"  value="'.$password.'">';
                         ?>
                       </div>
                     </div>
--- NiceAdmin/homepage.php
+++ NiceAdmin/homepage.php
@@ -258,98 +258,6 @@
         </ul>
       </li><!-- End Facilities Nav -->
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-journal-text"></i><span>Forms</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="forms-elements.html">
-              <i class="bi bi-circle"></i><span>Form Elements</span>
-            </a>
-          </li>
-          <li>
-            <a href="forms-layouts.html">
-              <i class="bi bi-circle"></i><span>Form Layouts</span>
-            </a>
-          </li>
-          <li>
-            <a href="forms-editors.html">
-              <i class="bi bi-circle"></i><span>Form Editors</span>
-            </a>
-          </li>
-          <li>
-            <a href="forms-validation.html">
-              <i class="bi bi-circle"></i><span>Form Validation</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Forms Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-layout-text-window-reverse"></i><span>Tables</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="tables-general.html">
-              <i class="bi bi-circle"></i><span>General Tables</span>
-            </a>
-          </li>
-          <li>
-            <a href="tables-data.html">
-              <i class="bi bi-circle"></i><span>Data Tables</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Tables Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-bar-chart"></i><span>Charts</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="charts-chartjs.html">
-              <i class="bi bi-circle"></i><span>Chart.js</span>
-            </a>
-          </li>
-          <li>
-            <a href="charts-apexcharts.html">
-              <i class="bi bi-circle"></i><span>ApexCharts</span>
-            </a>
-          </li>
-          <li>
-            <a href="charts-echarts.html">
-              <i class="bi bi-circle"></i><span>ECharts</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Charts Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-gem"></i><span>Icons</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="icons-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="icons-bootstrap.html">
-              <i class="bi bi-circle"></i><span>Bootstrap Icons</span>
-            </a>
-          </li>
-          <li>
-            <a href="icons-remix.html">
-              <i class="bi bi-circle"></i><span>Remix Icons</span>
-            </a>
-          </li>
-          <li>
-            <a href="icons-boxicons.html">
-              <i class="bi bi-circle"></i><span>Boxicons</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Icons Nav -->
-
       <li class="nav-heading">Pages</li>
 
       <li class="nav-item">
@@ -701,31 +609,31 @@
                       <tr>
                         <th scope="row"><a href="#"><img src="assets/img/Dewan-Azman-Hashim.png" alt=""></a></th>
                         <td><a href="#" class="text-primary fw-bold">Dewan Azham Hashim</a></td>
-                        <td>RM 275 / per hour</td>
+                        <td>RM 4000 / per day</td>
                         <td class="fw-bold">Available</td>
                       </tr>
                       <tr>
                         <th scope="row"><a href="#"><img src="assets/img/Bilik-Ilmuan-2.png" alt=""></a></th>
                         <td><a href="#" class="text-primary fw-bold">Bilik Ilmuan 2</a></td>
-                        <td>RM125 / per hour</td>
+                        <td>RM1700 / per day</td>
                         <td class="fw-bold">Available</td>
                       </tr>
                       <tr>
                         <th scope="row"><a href="#"><img src="assets/img/Bilik-Ilmuan-3.png" alt=""></a></th>
                         <td><a href="#" class="text-primary fw-bold">Bilik Ilmuan 3</a></td>
-                        <td>RM 125 / per hour</td>
+                        <td>RM 1200 / per day</td>
                         <td class="fw-bold">Unavailable</td>
                       </tr>
                       <tr>
                         <th scope="row"><a href="#"><img src="assets/img/BasketBallCourt.png" alt=""></a></th>
                         <td><a href="#" class="text-primary fw-bold">Basketball Court</a></td>
-                        <td>RM 90 / per hour</td>
+                        <td>RM 300 / per day</td>
                         <td class="fw-bold">Available</td>
                       </tr>
                       <tr>
                         <th scope="row"><a href="#"><img src="assets/img/TennisCourt.png" alt=""></a></th>
                         <td><a href="#" class="text-primary fw-bold">Tennis Court</a></td>
-                        <td>RM 75 / per hour</td>
+                        <td>RM 300 / per day</td>
                         <td class="fw-bold">Unavailable</td>
                       </tr>
                     </tbody>
@@ -766,7 +674,7 @@
                   <div class="activite-label">32 min</div>
                   <i class='bi bi-circle-fill activity-badge text-success align-self-start'></i>
                   <div class="activity-content">
-                    Quia quae rerum <a href="#" class="fw-bold text-dark">explicabo officiis</a> beatae
+                    Bilik ilmuan 3 is closed for maintenance.
                   </div>
                 </div><!-- End activity item-->
 
@@ -774,7 +682,7 @@
                   <div class="activite-label">56 min</div>
                   <i class='bi bi-circle-fill activity-badge text-danger align-self-start'></i>
                   <div class="activity-content">
-                    Voluptatem blanditiis blanditiis eveniet
+                    Tennis Court has been booked for 1 day.
                   </div>
                 </div><!-- End activity item-->
 
@@ -782,7 +690,7 @@
                   <div class="activite-label">2 hrs</div>
                   <i class='bi bi-circle-fill activity-badge text-primary align-self-start'></i>
                   <div class="activity-content">
-                    Voluptates corrupti molestias voluptatem
+                    User has send their enquiries through the chat box.
                   </div>
                 </div><!-- End activity item-->
 
@@ -790,7 +698,7 @@
                   <div class="activite-label">1 day</div>
                   <i class='bi bi-circle-fill activity-badge text-info align-self-start'></i>
                   <div class="activity-content">
-                    Tempore autem saepe <a href="#" class="fw-bold text-dark">occaecati voluptatem</a> tempore
+                    The payment RM500 has been made.
                   </div>
                 </div><!-- End activity item-->
 
@@ -798,7 +706,7 @@
                   <div class="activite-label">2 days</div>
                   <i class='bi bi-circle-fill activity-badge text-warning align-self-start'></i>
                   <div class="activity-content">
-                    Est sit eum reiciendis exercitationem
+                      Dewan Seminar has been booked for 1 day.
                   </div>
                 </div><!-- End activity item-->
 
@@ -806,7 +714,7 @@
                   <div class="activite-label">4 weeks</div>
                   <i class='bi bi-circle-fill activity-badge text-muted align-self-start'></i>
                   <div class="activity-content">
-                    Dicta dolorem harum nulla eius. Ut quidem quidem sit quas
+                      Dewan Jumaah is closed for maintenance.
                   </div>
                 </div><!-- End activity item-->
 
